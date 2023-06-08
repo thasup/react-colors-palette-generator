b1f56e8ceeb8af9a2e792a6623616287fdaaf59d
thasup <thanachonfirst@hotmail.com>
2023-06-09 03:44:29 +0700

    Initialize GA

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import ReactGA from "react-ga";
 import { Helmet } from "react-helmet";
 
 import Logo from "@/assets/logo.png";
@@ -7,6 +8,9 @@ import HelloWorld from "@/components/HelloWorld/HelloWorld";
 import styles from "./App.module.css";
 
 export default function App() {
+  const TRACKING_ID = "G-K2CTP3W6Q8";
+  ReactGA.initialize(TRACKING_ID);
+
   return (
     <Fragment>
       <Helmet>
