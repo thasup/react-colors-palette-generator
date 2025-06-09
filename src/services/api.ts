129fa800f0d7499b588c620acf89a650294ab0bc
thasup <thanachonfirst@hotmail.com>
2025-06-09 13:13:48 +0700

    task: Update API endpoint to new server URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -11,18 +11,15 @@ const postPrompt = async (query: string): Promise<PromptResponse> => {
   if (!query) {
     throw new Error("Missing a query");
   }
-  const response = await fetch(
-    "https://general-chatgpt-server.onrender.com/v1/colors/chat",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: new URLSearchParams({
-        input: query,
-      }),
-    }
-  );
+  const response = await fetch("https://api.thanachon.com/v1/colors/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      input: query,
+    }),
+  });
 
   const data = response.json();
 
